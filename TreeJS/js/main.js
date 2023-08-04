b787f613f3c85c967bfbb8a73e09938e92bc5bd5
Freddy Garcia <fgarcia@tockall.com>
2023-08-04 11:49:27 -0500

    Actualización de funcionalidad y modelos

--- TreeJS/js/main.js
+++ TreeJS/js/main.js
@@ -1,129 +1,91 @@
-//Import the THREE.js library
-import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-// To allow for the camera to move around the scene
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-// To allow for importing the .gltf file
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
-// To allow for importing the .fbx file
-import { AnimationMixer } from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
+import { FBXLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/FBXLoader.js';
+
+let camera, scene, renderer, stats;
+
+			const clock = new THREE.Clock();
+
+			let mixer;
+			
+			init();
+			animate();
+
+			function init() {
+
+				camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+				camera.position.set( 100, 300, 400 );
+
+				scene = new THREE.Scene();// Create Scene
+			
+				const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 ); //Set Lights
+				hemiLight.position.set( 0, 200, 0 );
+				scene.add( hemiLight );
+
+				const dirLight = new THREE.DirectionalLight( 0xffffff );
+				dirLight.position.set( 100, 40, 20 );
+				scene.add( dirLight );
+
+				const loader = new FBXLoader(); //Load 3D object
+				loader.load( './models/dancing.fbx', function ( object ) {
+
+					mixer = new THREE.AnimationMixer( object );
+
+					const action = mixer.clipAction( object.animations[ 0 ] );
+					action.play();
+
+					object.traverse( function ( child ) {
+
+						if ( child.isMesh ) {
+
+							child.castShadow = true;
+							child.receiveShadow = true;
+
+						}
+
+					} );
+					const position = new THREE.Vector3(0, 200, 0);
+					scene.position.copy(position);
+					scene.add( object );
+
+				} );
+				const audioLoader = new THREE.AudioLoader(); // Set the Audio
+				const listener = new THREE.AudioListener();
+				const audio = new THREE.Audio(listener);
+				audio.muted = true;
+				audioLoader.load('./audio/salsa.mp3', function(buffer) {
+					audio.setBuffer(buffer);
+					audio.setLoop(true); 
+					audio.play(); 
+					audio.setVolume(0.2); 
+				  });
+				 
+				renderer = new THREE.WebGLRenderer( { alpha: true } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.shadowMap.enabled = true;
+				document.getElementById("container3D").appendChild(renderer.domElement);
+				camera.add(listener);
+				/*const controls = new OrbitControls( camera, renderer.domElement );
+				controls.target.set( 0, 0, 0 );
+				controls.update();*/
+				window.addEventListener( 'resize', onWindowResize );
+			}
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
 
 
-//Create a Three.JS Scene
-const scene = new THREE.Scene();
-//create a new camera with positions and angles
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+			function animate() {
 
-//Keep track of the mouse position, so we can make the eye move
-let mouseX = window.innerWidth / 2;
-let mouseY = window.innerHeight / 2;
+				requestAnimationFrame( animate );
+				const delta = clock.getDelta();
+				if ( mixer ) mixer.update( delta );
+				renderer.render( scene, camera );
 
-//Keep the 3D object on a global variable so we can access it later
-let object;
-
-//OrbitControls allow the camera to move around the scene
-let controls;
-
-//Set which object to render
-let objToRender = 'eye';
-
-let mixer;
-let animationAction;
-let prevTime = 0;
-
-
-//Instantiate a loader for the .gltf file
-const loader = new GLTFLoader();
-
-//Load the file
-loader.load(
-  `models/${objToRender}/scene.gltf`,
-  function (gltf) {
-    //If the file is loaded, add it to the scene
-    object = gltf.scene;
-    scene.add(object);
-
-    // Configurar el AnimationMixer
-    mixer = new AnimationMixer(object);
-
-    // Obtener la acción de la animación (assumimos que la animación es la primera del array)
-    animationAction = mixer.clipAction(gltf.animations[1]);
-
-    // Reproducir la animación en loop
-    animationAction.play();
-    console.log(gltf.animations);
-  },
-  function (xhr) {
-    //While it is loading, log the progress
-    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-  },
-  function (error) {
-    //If there is an error, log it
-    console.error(error);
-  }
-);
-
-//Instantiate a new renderer and set its size
-const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-//Add the renderer to the DOM
-document.getElementById("container3D").appendChild(renderer.domElement);
-
-//Set how far the camera will be from the 3D model
-camera.position.z = objToRender === "dino" ? 25 : 500;
-
-//Add lights to the scene, so we can actually see the 3D model
-const topLight = new THREE.DirectionalLight(0xffffff, 1.3); // (color, intensity)
-topLight.position.set(1, 2, 3) //top-left-ish
-topLight.castShadow = true;
-scene.add(topLight);
-
-const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
-scene.add(ambientLight);
-/*
-//This adds controls to the camera, so we can rotate / zoom it with the mouse
-if (objToRender === "dino") {
-  controls = new OrbitControls(camera, renderer.domElement);
-}*/
-
-//Render the scene
-function animate() {
-  requestAnimationFrame(animate);
-  //Here we could add some code to update the scene, adding some automatic movement
-  // Calculate the time elapsed since the last frame in seconds
-  const time = performance.now();
-  const deltaTime = (time - prevTime) * 0.001; // Convert to seconds
-  prevTime = time;
-
-  // Update the AnimationMixer with the time elapsed
-  if (mixer) {
-   mixer.update(deltaTime);
-  }
-  //Make the eye move
-  /*if (object && objToRender === "eye") {
-    //I've played with the constants here until it looked good 
-    object.rotation.y = -3 + mouseX / window.innerWidth * 3;
-    object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
-  }*/
-  renderer.render(scene, camera);
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.enableRotate = true; // Habilitar la rotación con el mouse
-  controls.enablePan = false; // Deshabilitar el pan (movimiento) con el mouse
-  controls.enableZoom = false; // Deshabilitar el zoom con el mouse
-}
-
-//Add a listener to the window, so we can resize the window and the camera
-window.addEventListener("resize", function () {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
-
-//add mouse position listener, so we can make the eye move
-document.onmousemove = (e) => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-}
-
-//Start the 3D rendering
-animate();
+			}
